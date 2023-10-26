3086b6b19d65280d86053f90a1ac2373949929ab
Azalea9X <53317393+Azalea9X@users.noreply.github.com>
2023-10-25 21:50:26 -0400

    Delete test-backend/express_test directory

--- test-backend/express_test/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import {render} from "react-dom"; 
-import {BrowserRouter as Router} from "react-router-dom"; 
-
-render(<Router><App/></Router>, document.getElementById("root"));
