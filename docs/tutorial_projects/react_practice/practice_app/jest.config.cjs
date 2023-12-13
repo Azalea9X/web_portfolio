a679dd30e27f23aea95ada45f1b154e60cdf09fc
Jake <Azalea9x@gmail.com>
2023-12-13 02:53:00 -0500

    Working v.001

--- docs/tutorial_projects/react_practice/practice_app/jest.config.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "testEnvironment": "jsdom"
-}
-
-//https://stackoverflow.com/questions/69227566/consider-using-the-jsdom-test-environment
--- docs/tutorial_projects/react_practice/practice_app/readme.md
+++
@@ -1,1 +0,0 @@
-Got help from: https://dev.to/hannahadora/jest-testing-with-vite-and-react-typescript-4bap
